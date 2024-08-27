a5437b75f9e5292aeb98bb607adb76db0b146d1a
Aman Bhardwaj <amanbhardwaj12072003@gmail.com>
2024-08-27 15:11:23 +0530

    Created ListenAndAccept to listen and accept the tcp transport

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -1,22 +1,45 @@
 package p2p
 
 import (
+	"fmt"
 	"net"
 	"sync"
 )
 
-type TCPTransport struct{
+type TCPTransport struct {
 	listenAddress string
-	listner net.Listener
+	listner       net.Listener
 
-	mu sync.RWMutex
+	mu    sync.RWMutex
 	peers map[net.Addr]Peer
 }
 
-func NewTCPTransport(listenAddr string) *TCPTransport{
+func NewTCPTransport(listenAddr string) *TCPTransport {
 	return &TCPTransport{
 		listenAddress: listenAddr,
 	}
 }
 
+func (t *TCPTransport) ListenAndAccept() error {
+	var err error
+	t.listner, err = net.Listen("tcp", t.listenAddress)
+	if err != nil {
+		return err
+	}
+	go t.startAcceptLoop()
+	return nil
+}
 
+func (t *TCPTransport) startAcceptLoop() {
+	for {
+		conn, err := t.listner.Accept()
+		if err != nil {
+			fmt.Printf("TCP Accept Error: %s\n", err)
+		}
+		go t.handleConn(conn)
+	}
+}
+
+func (t *TCPTransport) handleConn(conn net.Conn) {
+	fmt.Printf("new incoming connection: %+v\n", conn)
+}
