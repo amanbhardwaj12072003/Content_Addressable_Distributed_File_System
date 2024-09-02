e7a3ea613b2f87cd78379cebee224aac0664979f
Aman Bhardwaj <amanbhardwaj12072003@gmail.com>
2024-09-02 21:26:56 +0530

    Added test logic for Read() and readStream()

--- store_test.go
+++ store_test.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"io/ioutil"
 	"testing"
 )
 
 func TestPathTransformFunc(t *testing.T) {
 	key := "dogsbestpicture"
 	pathKey := CASPathTransformFunc(key)
-	expectedOriginalKey := "b519d9dda53fe3a9c3f1aca305b98a6f7d8/10a95"
+	expectedOriginalKey := "b519d9dda53fe3a9c3f1aca305b98a6f7d810a95"
 	expectedPathName := "b519d/9dda5/3fe3a/9c3f1/aca30/5b98a/6f7d8/10a95"
 	if pathKey.PathName != expectedPathName {
 		t.Errorf("have: %s but want: %s", pathKey.PathName, expectedPathName)
@@ -24,9 +26,21 @@ func TestStore(t *testing.T) {
 	}
 	s := NewStore(opts)
 	key := "myspecialpicture"
+	data := []byte("some jpg bytes")
 
-	data := bytes.NewReader([]byte("some jpg bytes"))
-	if err := s.writeStream(key, data); err != nil {
+	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
 		t.Error(err)
+	}
+
+	r, err := s.Read(key)
+	if err != nil {
+		t.Error(err)
+	}
+
+	b, _ := ioutil.ReadAll(r)
+	fmt.Print(string(b))
+
+	if string(b) != string(data) {
+		t.Errorf("want: %s but have: %s", data, b)
 	}
 }
