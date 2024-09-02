ffed75741ac582aee2a8519c93ec630fcffab0c5
Aman Bhardwaj <amanbhardwaj12072003@gmail.com>
2024-09-02 14:39:49 +0530

    Added TestPathTransformFunc to test PathTransformFunc

--- store_test.go
+++ store_test.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"testing"
 )
 
+func TestPathTransformFunc(t *testing.T) {
+	key := "dogsbestpicture"
+	pathname := CASPathTransformFunc(key)
+	expectedPathName := "b519d/9dda5/3fe3a/9c3f1/aca30/5b98a/6f7d8/10a95"
+	if pathname != expectedPathName {
+		t.Errorf("have: %s but want: %s", pathname, expectedPathName)
+	}
+	fmt.Println(pathname)
+}
+
 func TestStore(t *testing.T) {
 	opts := StoreOpts{
-		PathTransformFunc: DefaultPathTransformFunc,
+		PathTransformFunc: CASPathTransformFunc,
 	}
 	s := NewStore(opts)
 
