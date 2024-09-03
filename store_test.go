62c4d6445445cc1dd003e4ab6a50e4aa7aac1b67
Aman Bhardwaj <amanbhardwaj12072003@gmail.com>
2024-09-03 13:31:44 +0530

    Added TestStoreDelete to test Delete(key) function

--- store_test.go
+++ store_test.go
@@ -20,6 +20,23 @@ func TestPathTransformFunc(t *testing.T) {
 	}
 }
 
+func TestStoreDelete(t *testing.T) {
+	opts := StoreOpts{
+		PathTransformFunc: CASPathTransformFunc,
+	}
+	s := NewStore(opts)
+	key := "myspecialpicture"
+	data := []byte("some jpg bytes")
+
+	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
+		t.Error(err)
+	}
+
+	if err := s.Delete(key); err != nil {
+		t.Error(err)
+	}
+}
+
 func TestStore(t *testing.T) {
 	opts := StoreOpts{
 		PathTransformFunc: CASPathTransformFunc,
