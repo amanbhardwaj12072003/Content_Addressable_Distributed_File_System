ec883d28e74cecec34a0332b4391fa49ffe92cf1
Aman Bhardwaj <amanbhardwaj12072003@gmail.com>
2024-09-06 16:40:23 +0530

    Final Commit : Error Resolved - All The 3 Ports Working

--- main.go
+++ main.go
@@ -35,12 +35,13 @@ func makeServer(listenAddr string, nodes ...string) *FileServer {
 
 func main() {
 	server_1 := makeServer(":3000", "")
-	server_2 := makeServer(":6000", "")
-	server_3 := makeServer(":5000", ":3000", ":6000")
+	server_2 := makeServer(":3001", "")
+	server_3 := makeServer(":3002", ":3000", ":3001")
 
 	go func() { log.Fatal(server_1.Start()) }()
 	time.Sleep(2 * time.Second)
 	go func() { log.Fatal(server_2.Start()) }()
+	time.Sleep(2 * time.Second)
 
 	go server_3.Start()
 	time.Sleep(2 * time.Second)
@@ -48,7 +49,7 @@ func main() {
 	for i := 0; i < 10; i++ {
 
 		key := fmt.Sprintf("cowpicture_%d.jpg", i)
-		// key := "catspicture.jpg"
+		// key := "cowpicture.jpg"
 		data := bytes.NewReader([]byte("my big data file here!"))
 		server_3.Store(key, data)
 
@@ -87,25 +88,29 @@ func main() {
 	go server_2.Start()
 	time.Sleep(1 * time.Second)
 
-	key := "catspicture.jpg"
-	data := bytes.NewReader([]byte("my big data file here!"))
-	server_2.Store(key, data)
+	for i := 0; i < 10; i++ {
 
-	if err := server_2.store.Delete(key); err != nil {
-		log.Fatal(err)
+		key := fmt.Sprintf("catspicture_%d.jpg", i)
+		// key := "catspicture.jpg"
+		data := bytes.NewReader([]byte("my big data file here!"))
+		server_2.Store(key, data)
+
+		if err := server_2.store.Delete(server_2.ID, key); err != nil {
+			log.Fatal(err)
+		}
+
+		r, err := server_2.Get(key)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		b, err := ioutil.ReadAll(r)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(string(b))
 	}
-
-	r, err := server_2.Get(key)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	b, err := ioutil.ReadAll(r)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(string(b))
 }
 
 */
